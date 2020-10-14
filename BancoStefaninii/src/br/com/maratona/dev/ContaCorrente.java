2274a55cdac02b00c92911a9392f7d27518236b8
luanaostergren <69064081+luanaostergren@users.noreply.github.com>
2020-10-14 18:25:10 -0300

    Add files via upload

---
+++ BancoStefaninii/src/br/com/maratona/dev/ContaCorrente.java
@@ -0,0 +1,14 @@
+package br.com.maratona.dev;
+
+public class ContaCorrente extends Conta {
+	
+	private Float limite;
+
+	public Float getLimite() {
+		return limite;
+	}
+
+	public void setLimite(Float limite) {
+		this.limite = limite;
+	}
+}
